8771696e26073d541fd56cdc29f9387783d27dfb
Manuel Zamalloa LL <mzamalloa97@gmail.com>
2025-09-04 13:39:42 -0500

    exercise 2 complete

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,55 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("Grade percentage: ");
+        int percentage = int.Parse(Console.ReadLine());
+        string letter;
+        string sign;
+
+        if (percentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (percentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (percentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (percentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        if (percentage % 10 >= 7)
+        {
+            sign = "+";
+        }
+        else if (percentage % 10 < 3)
+        {
+            sign = "-";
+        }
+        else
+        {
+            sign = "";
+        }
+
+        if (percentage >= 93 || percentage < 60)
+        {
+            sign = ""; 
+        }
+
+        Console.WriteLine($"Your grade is: {letter}{sign}");
+
+        if (percentage >= 70)
+        {
+            Console.WriteLine("Congratulations you passed the course!!");
+        }
     }
 }
