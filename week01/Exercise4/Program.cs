447324284f93c7bd83f5d38c226ce547075c2a8c
Manuel Zamalloa LL <mzamalloa97@gmail.com>
2025-09-04 22:15:50 -0500

    exercise 4 complete

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numberList = new List<int>();
+        int number = 1;
+        int total = 0;
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        while (number != 0)
+        {
+            Console.Write("Enter number: ");
+            number = int.Parse(Console.ReadLine());
+            if (number != 0)
+            {
+                numberList.Add(number);
+            }
+        }
+
+        foreach (int num in numberList)
+        {
+            total += num;
+        }
+
+        float average = (float)total / numberList.Count;
+        int largestNum = numberList.Max(); // found through ai that you can use MAX() function 
+
+        Console.WriteLine($"The sum is: {total}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {largestNum}");
+
     }
 }
