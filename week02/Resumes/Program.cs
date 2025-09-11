e3625576827969148095b3c9f8165e2366b4d9bf
Manuel Zamalloa LL <mzamalloa97@gmail.com>
2025-09-11 11:21:38 -0500

    completed resume

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -10,6 +10,19 @@ class Program
         job1._startYear = 2024;
         job1._endYear = 2025;
 
+        Job job2 = new Job();
+        job2._company = "Microsoft";
+        job2._jobTitle = "Software Engineer";
+        job2._startYear = 2021;
+        job2._endYear = 2022;
+
+        Resume myResume = new Resume();
+        myResume._name = "James";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
         job1.Display();
+        myResume.Display();
+
     }
 }
