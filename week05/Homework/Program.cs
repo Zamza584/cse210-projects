8ea7f0a5c5edf3e2420ae699c12350920f0d34a8
Manuel Zamalloa LL <mzamalloa97@gmail.com>
2025-10-02 12:35:38 -0500

    Assignments

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -1,9 +1,20 @@
 using System;
+using homework;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment assignment = new Assignment("James Stewart", "Algebra");
+        Console.WriteLine(assignment.GetSummary());
+
+        MathAssignment mathAssignment = new MathAssignment("3.4", "3-4", "james Reed", "Algebra");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        WritingAssignment writingAssignment = new WritingAssignment("The end of a century", "Fran Monk", "History");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
+
     }
 }
